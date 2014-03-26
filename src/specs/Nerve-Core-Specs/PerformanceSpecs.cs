26ef17a425f9ea64d6f358d41d90f2d1ccfe2a5c
kostassoid <k.alexandroff@gmail.com>
2014-03-26 15:21:25 +0400

    Disabled performance tests.

--- src/specs/Nerve-Core-Specs/PerformanceSpecs.cs
+++ src/specs/Nerve-Core-Specs/PerformanceSpecs.cs
@@ -40,18 +40,19 @@ namespace Kostassoid.Nerve.Core.Specs
 
 			protected static ICell Cell;
 
-			private It should_be_faster_than_0_5_million_ops = () =>
+            [Ignore("Not stable for CI environment.")]
+            private It should_be_faster_than_0_5_million_ops = () =>
 				{
 					var countdown = new CountdownEvent(SignalsCount);
 					Cell.OnStream().Of<Ping>().ReactWith(_ => countdown.Signal());
 
-					Stopwatch stopwatch = Stopwatch.StartNew();
+					var stopwatch = Stopwatch.StartNew();
 					Enumerable.Range(0, SignalsCount).ForEach(_ => Cell.Send(new Ping()));
 
 					countdown.Wait();
 					stopwatch.Stop();
 
-					long ops = SignalsCount * 1000L / stopwatch.ElapsedMilliseconds;
+					var ops = SignalsCount * 1000L / stopwatch.ElapsedMilliseconds;
 					Console.WriteLine("Ops / second: {0}", ops);
 					ops.ShouldBeGreaterThan(500000);
 				};
@@ -65,11 +66,11 @@ namespace Kostassoid.Nerve.Core.Specs
 
 			protected static ICell Cell;
 
-			private Behaves_like<fast_message_broker> _;
+			Behaves_like<fast_message_broker> _;
 
-			private Cleanup after = () => Cell.Dispose();
+			Cleanup after = () => Cell.Dispose();
 
-			private Establish context = () => { Cell = new Cell(ImmediateScheduler.Factory); };
+			Establish context = () => { Cell = new Cell(ImmediateScheduler.Factory); };
 		}
 
 		[Subject(typeof(Cell), "Performance")]
@@ -80,11 +81,11 @@ namespace Kostassoid.Nerve.Core.Specs
 
 			protected static ICell Cell;
 
-			private Behaves_like<fast_message_broker> _;
+			Behaves_like<fast_message_broker> _;
 
-			private Cleanup after = () => Cell.Dispose();
+			Cleanup after = () => Cell.Dispose();
 
-			private Establish context = () => { Cell = new Cell(PoolScheduler.Factory); };
+			Establish context = () => { Cell = new Cell(PoolScheduler.Factory); };
 		}
 
 		[Subject(typeof(Cell), "Performance")]
@@ -95,11 +96,11 @@ namespace Kostassoid.Nerve.Core.Specs
 
 			protected static ICell Cell;
 
-			private Behaves_like<fast_message_broker> _;
+			Behaves_like<fast_message_broker> _;
 
-			private Cleanup after = () => Cell.Dispose();
+			Cleanup after = () => Cell.Dispose();
 
-			private Establish context = () => { Cell = new Cell(ThreadScheduler.Factory); };
+			Establish context = () => { Cell = new Cell(ThreadScheduler.Factory); };
 		}
 	}
 
